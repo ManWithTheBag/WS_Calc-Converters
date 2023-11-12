83eda8681f560de98a948cb273ee8657b2a2a460
Serhii <107812569+ManWithTheBag@users.noreply.github.com>
2023-11-12 01:10:46 -0500

    Test commit 2

---
+++ src/test/java/org/calc_converter/ws_calcconverter/WsCalcConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package org.calc_converter.ws_calcconverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsCalcConverterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
