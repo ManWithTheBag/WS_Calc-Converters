381ae8e493c41e8356e3a8996aa9aa24ea9f0550
Serhii <107812569+ManWithTheBag@users.noreply.github.com>
2023-11-12 04:19:22 -0500

    Test commit 3

---
+++ src/main/java/org/calc_converter/ws_calcconverter/controller/MainController.java
@@ -0,0 +1,23 @@
+package org.calc_converter.ws_calcconverter.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.view.RedirectView;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public RedirectView mainPage() {
+        return new RedirectView("/calculatorsList");
+    }
+
+    @GetMapping("/termsAndConditions")
+    public String termsConditions(){
+        return "termsAndConditions";
+    }
+    @GetMapping("/calculatorsList")
+    public String calculatorsList() {
+        return "calculatorsList";
+    }
+}
